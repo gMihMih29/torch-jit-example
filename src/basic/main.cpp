f92e52e4596fcf4696455cc0b0b818bcbe60c87e
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-02-07 14:27:48 +0300

    feat: implemented test function for model without control flow

--- src/basic/main.cpp
+++ src/basic/main.cpp
@@ -1,28 +1,60 @@
 #include <torch/script.h>
 #include <torch/torch.h>
-#include <opencv4/opencv2/opencv.hpp>
 
 #include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include <string>
+#include <timer/Timer.hpp>
 
-int main() {
-    std::string model_path = "model.pt";
-    torch::jit::script::Module model;
+void test_my_model() {
+    Timer t;
+    std::string scripted_model_path = "./models/scripted_MyModel.pt";
+    std::string traced_model_path = "./models/traced_MyModel.pt";
+    torch::jit::script::Module scripted_model, traced_model;
+    // Scripted
     try {
-        model = torch::jit::load(model_path);
-        std::cout << "Model was loaded successful!" << std::endl;
-
-        std::vector<float> input_data(10, 1.0);
-        torch::Tensor input_tensor = torch::from_blob(input_data.data(), {1, 10});
-
-        std::vector<torch::jit::IValue> inputs;
-        inputs.push_back(input_tensor);
-        at::Tensor output = model.forward(inputs).toTensor();
-
-        std::cout << "Model output: " << output << std::endl;
+        scripted_model = torch::jit::load(scripted_model_path);
     } catch (const c10::Error &e) {
         std::cerr << "Error loading the model: " << e.what() << std::endl;
-        return -1;
+        return;
     }
+    std::cout << "scripted_model was loaded successfully!\n";
+    torch::Tensor input_tensor = torch::randn({1, 10});
+    std::cout << "Input tensor: " << input_tensor << std::endl;
 
+    std::vector<torch::jit::IValue> inputs;
+    inputs.push_back(input_tensor);
+    t.Reset();
+    auto output1 = scripted_model.forward(inputs).toTensor();
+    auto time1 = t.GetMicroseconds();
+    std::cout << "Output: " << output1 << std::endl << "Elaplsed: " << time1 << std::endl;
+
+    std::cout << "\n\n\n";
+    // Traced
+    try {
+        traced_model = torch::jit::load(traced_model_path);
+    } catch (const c10::Error &e) {
+        std::cerr << "Error loading the model: " << e.what() << std::endl;
+        return;
+    }
+    std::cout << "traced_model was loaded successfully!\n";
+    input_tensor = torch::randn({1, 10});
+    std::cout << "Input tensor: " << input_tensor << std::endl;
+
+    inputs.clear();
+    inputs.push_back(input_tensor);
+    t.Reset();
+    auto output2 = traced_model.forward(inputs).toTensor();
+    auto time2 = t.GetMicroseconds();
+    std::cout << "Output: " << output2 << std::endl << "Elaplsed: " << time2 << std::endl;
+}
+
+void test_my_model_with_control_flow() {
+    
+}
+
+int main() {
+    test_my_model();
+    test_my_model_with_control_flow();
     return 0;
 }
