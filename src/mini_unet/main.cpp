f9411281a3acebb055a7ad5cbc78d6306cf008a2
Mikhail Getmanov <mmgetmanov@gmail.com>
2025-02-10 11:30:24 +0300

    feat: trying to fix strange results of mini_unet

--- src/mini_unet/main.cpp
+++ src/mini_unet/main.cpp
@@ -8,38 +8,35 @@
 
 torch::Tensor transform_image(const cv::Mat& image, int height, int width) {
 
-    // Resize the image using OpenCV
     cv::Mat resized_image;
     cv::resize(image, resized_image, cv::Size(width, height), 0, 0, 0);
-    // Convert back to tensor
     torch::Tensor out = torch::from_blob(resized_image.data, {1, 3, height, width}, torch::kInt8).contiguous();
     // torch::Tensor out = torch::from_blob(resized_image.data, {1, 3, height, width}, torch::kInt8).permute({0, 1, 2, 3}).contiguous();
-    return out.to(torch::kFloat32).clone();
+    return (out.to(torch::kFloat32).clone() + 128) / 255.f;
 }
 
 cv::Mat tensor_to_mat(const torch::Tensor& tensor) {
-    // Ensure the tensor is on the CPU and in the correct type
-    auto tensor_cpu = tensor.to(torch::kCPU).to(torch::kU8);
-    
-    // Get the tensor dimensions (C x H x W)
+    auto tensor_cpu = (tensor * 255.0f).to(torch::kUInt8);
+
     int height = tensor_cpu.size(2);
     int width = tensor_cpu.size(3);
 
-    // Create an OpenCV Mat with the correct size and type
     cv::Mat mat(cv::Size(width, height), CV_8UC1, tensor_cpu.data_ptr<uchar>());
 
-    // OpenCV uses BGR format, so we need to reorder the channels if the tensor is RGB
     cv::cvtColor(mat, mat, cv::COLOR_RGB2BGR);
 
     return mat;
 }
 
 void test_model(torch::jit::script::Module& model, std::string prefix_out_name) {
+    float threshold = 0.5f;
+
     cv::Mat image_10 = cv::imread("dataset/image/10.jpg");
     auto transformed_img = transform_image(image_10, 224, 224);
     std::vector<torch::jit::IValue> inputs;
     inputs.push_back(transformed_img);
     auto output = model.forward(inputs).toTensor();
+    output = ((output / torch::max(output).item()) > threshold);
     auto output_img = tensor_to_mat(output);
     cv::imwrite(prefix_out_name + "_out_10.jpg", output_img);
 
@@ -48,6 +45,8 @@ void test_model(torch::jit::script::Module& model, std::string prefix_out_name) 
     std::vector<torch::jit::IValue> inputs11;
     inputs11.push_back(transformed_img11);
     auto output11 = model.forward(inputs11).toTensor();
+    // output11 = ((output11 / torch::max(output11).item()) > threshold).to(torch::kFloat32);
+    output11 = ((output11 / torch::max(output11).item()) > threshold);
     auto output_img11 = tensor_to_mat(output11);
     cv::imwrite(prefix_out_name + "_out_11.jpg", output_img11);
 
@@ -56,6 +55,7 @@ void test_model(torch::jit::script::Module& model, std::string prefix_out_name) 
     std::vector<torch::jit::IValue> inputs23;
     inputs23.push_back(transformed_img23);
     auto output23 = model.forward(inputs23).toTensor();
+    output23 = ((output23 / torch::max(output23).item()) > threshold);
     auto output_img23 = tensor_to_mat(output23);
     cv::imwrite(prefix_out_name + "_out_23.jpg", output_img23);
 }
